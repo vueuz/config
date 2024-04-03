97d78bdbc2b078d553d75ee70742699ceb96bdc8
Yung Venuz <129729074+venuz@users.noreply.github.com>
2024-04-03 15:40:00 +0800

    update config

---
+++ zsh/mappings.zsh
@@ -0,0 +1,22 @@
+function zle_eval {
+    echo -en "\e[2K\r"
+    eval "$@"
+    zle redisplay
+}
+
+function openlazygit {
+    zle_eval lazygit
+}
+
+zle -N openlazygit; bindkey "^G" openlazygit
+
+function openlazynpm {
+    zle_eval lazynpm
+}
+
+zle -N openlazynpm; bindkey "^N" openlazynpm
+
+
+# AUTO-COMPLETION
+bindkey '^y' autosuggest-accept
+bindkey '^e' autosuggest-execute
